31683d684b65bde0137e15844a076cb7599b3fee
Steven Ko <chunyuko85@gmaill.com>
2013-06-13 10:59:20 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FashionServer::Application.config.secret_token = '0aa40d1c18499b2609af7549efa6736e2229fc46c168af35e0472700c0ad562207b83fade99f9f9ec46ed61e05221740c461080392c9b355dafc1d039680bc38'
